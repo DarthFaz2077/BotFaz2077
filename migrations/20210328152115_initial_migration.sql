23f1f2d4a9d475d685bb029f820a38327d6229a4
DarthFaz2077 <68593607+DarthFaz2077@users.noreply.github.com>
2021-03-28 17:44:04 +0200

    Changed initial migration.

---
+++ migrations/20210328152115_initial_migration.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS public.users
+(
+    user_id BIGINT NOT NULL,
+    level INT NOT NULL DEFAULT 0,
+    total_xp INT NOT NULL DEFAULT 0,
+    current_xp INT NOT NULL DEFAULT 0,
+    coins BIGINT NOT NULL DEFAULT 0,
+    daily_date DATE,
+    CONSTRAINT user_pkey PRIMARY KEY (user_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.users
+    OWNER to postgres;
