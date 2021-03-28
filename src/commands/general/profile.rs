b95b47df04094f4f5d1f55a41c472ce548103a00
DarthFaz2077 <68593607+DarthFaz2077@users.noreply.github.com>
2021-03-28 15:36:33 +0200

    Added mentions to profile command.

--- src/commands/general/profile.rs
+++ src/commands/general/profile.rs
@@ -8,41 +8,99 @@ use serenity::{
 use sqlx::query;
 
 #[command]
-#[description = "Show your own profile."]
+#[description = "Show the profiles of mentioned people, or your own."]
 #[example("")]
+#[example("@mention")]
+#[example("@mention1 @mention2")]
 async fn profile(ctx: &Context, msg: &Message) -> CommandResult {
     let data = ctx.data.read().await;
     let pg_pool = data.get::<PgPoolContainer>().cloned().unwrap();
 
-    let result = query!(
-        "SELECT level, current_xp FROM users WHERE user_id = $1",
-        msg.author.id.0 as i64
-    )
-    .fetch_one(&pg_pool)
-    .await
-    .unwrap();
-
-    msg.channel_id
-        .send_message(ctx, |m| {
-            m.embed(|e| {
-                e.title("Profile");
-                e.description(format!("{}'s profile.", msg.author.tag()));
-                e.field("XP:", format!("{}/100", result.current_xp), false);
-                e.field("Level:", result.level, false);
-                e.footer(|f| {
-                    f.text(format!("Requested by {}.", msg.author.tag()));
-                    f.icon_url(msg.author.face());
-
-                    f
-                });
-                e.timestamp(&Utc::now());
-
-                e
-            });
-
-            m
-        })
+    if msg.mentions.is_empty() {
+        let result = query!(
+            "SELECT level, current_xp FROM users WHERE user_id = $1",
+            msg.author.id.0 as i64
+        )
+        .fetch_one(&pg_pool)
         .await?;
+
+        msg.channel_id
+            .send_message(ctx, |m| {
+                m.embed(|e| {
+                    e.title("Profile");
+                    e.description(format!("{}'s profile.", msg.author.tag()));
+                    e.field("XP:", format!("{}/100", result.current_xp), false);
+                    e.field("Level:", result.level, false);
+                    e.footer(|f| {
+                        f.text(format!("Requested by {}.", msg.author.tag()));
+                        f.icon_url(msg.author.face());
+
+                        f
+                    });
+                    e.timestamp(&Utc::now());
+
+                    e
+                });
+
+                m
+            })
+            .await?;
+    } else {
+        for user in &msg.mentions {
+            match query!(
+                "SELECT level, current_xp FROM users WHERE user_id = $1",
+                user.id.0 as i64
+            )
+            .fetch_one(&pg_pool)
+            .await
+            {
+                Ok(result) => {
+                    msg.channel_id
+                        .send_message(ctx, |m| {
+                            m.embed(|e| {
+                                e.title("Profile");
+                                e.description(format!("{}'s profile.", user.tag()));
+                                e.field("XP:", format!("{}/100", result.current_xp), false);
+                                e.field("Level:", result.level, false);
+                                e.footer(|f| {
+                                    f.text(format!("Requested by {}.", msg.author.tag()));
+                                    f.icon_url(msg.author.face());
+
+                                    f
+                                });
+                                e.timestamp(&Utc::now());
+
+                                e
+                            });
+
+                            m
+                        })
+                        .await?;
+                }
+                Err(_) => {
+                    msg.channel_id
+                        .send_message(ctx, |m| {
+                            m.embed(|e| {
+                                e.title("Profile");
+                                e.description(format!("{} doesn't have a profile.", user.tag()));
+                                e.footer(|f| {
+                                    f.text(format!("Requested by {}.", msg.author.tag()));
+                                    f.icon_url(msg.author.face());
+
+                                    f
+                                });
+                                e.timestamp(&Utc::now());
+
+                                e
+                            });
+
+                            m
+                        })
+                        .await?;
+                }
+            }
+        }
+    }
 
     Ok(())
 }
