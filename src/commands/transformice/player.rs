91f45edf63207a207ebf85ce429084d0ca877c8e
DarthFaz2077 <68593607+DarthFaz2077@users.noreply.github.com>
2021-07-02 13:27:07 +0200

    Basic error handling in player command.

--- src/commands/transformice/player.rs
+++ src/commands/transformice/player.rs
@@ -161,6 +161,29 @@ async fn player(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
         response_json.shop.look, response_json.shop.mouse_color
     ))?;
     let response = reqwest_client.get(request_url).send().await?;
+    if !response.status().is_success() {
+        msg.channel_id
+            .send_message(ctx, |m| {
+                m.embed(|e| {
+                    e.title("Transformice Player Stats");
+                    e.description("Something happened!");
+                    e.footer(|f| {
+                        f.text(format!("Requested by {}.", msg.author.tag()));
+                        f.icon_url(msg.author.face());
+
+                        f
+                    });
+                    e.timestamp(&Utc::now());
+
+                    e
+                });
+
+                m
+            })
+            .await?;
+
+        return Ok(());
+    }
     let response_svg = response.text().await?;
 
     let mut pixmap: Pixmap;
